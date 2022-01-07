4913c37bef79fbf08dfade6bff7755d8f20f2509
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-01-07 10:16:50 +0200

    feat(edit-reservation)

--- modules/reservation/services/createReservation.js
+++ modules/reservation/services/createReservation.js
@@ -1,9 +1,15 @@
-const Car = require('../../../models/car');
+const Reservation = require('../../../models/reservation');
 
-const createReservation = async (body) => {
-  const reservation = await reservation.create(body);
-
-  return reservation;
-};
+const createReservation = async (resData) => {
+    const reservation = await Reservation.create({
+      res_id: resData.res_id,
+      car_id: resData.car_id,
+      customer_id: resData.customer_id,
+      res_date: resData.res_date,
+      return_date: resData.return_date,
+    });
+  
+    return reservation;
+  };
 
 module.exports = createReservation;
